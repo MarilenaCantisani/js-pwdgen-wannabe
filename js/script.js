9251ac50c0c5367424351c2a729f25694100bb1e
MarilenaCantisani <marilena.cantisani97@gmail.com>
2021-06-30 14:57:03 +0200

    + Added output to html

--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
 console.log("JS OK")
 
 // Variable
-var name = prompt ("Inserisci qui il tuo nome:")
-console.log(name)
+var name = prompt ("Inserisci qui il tuo nome:");
+console.log(name);
 
-var surname = prompt ("Inserisci qui il tuo cognome:")
-console.log(surname)
+var surname = prompt ("Inserisci qui il tuo cognome:");
+console.log(surname);
 
-var favoriteColor = prompt ("Inserisci qui il tuo colore preferito:")
-console.log(favoriteColor)
+var favoriteColor = prompt ("Inserisci qui il tuo colore preferito:");
+console.log(favoriteColor);
 
+var message = "La tua password è: ";
 
+var passwordPlaceholder = document.getElementById("password");
 
+passwordPlaceholder.innerHTML = message + name + surname + favoriteColor;
