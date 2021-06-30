ceef4e7cc3ca4cd2c93bf07e0a5c76db4da60393
MarilenaCantisani <marilena.cantisani97@gmail.com>
2021-06-30 14:45:29 +0200

    + Added var name / var surname / var favorite color

---
+++ js/script.js
@@ -0,0 +1,14 @@
+console.log("JS OK")
+
+// Variable
+var name = prompt ("Inserisci qui il tuo nome:")
+console.log(name)
+
+var surname = prompt ("Inserisci qui il tuo cognome:")
+console.log(surname)
+
+var favoriteColor = prompt ("Inserisci qui il tuo colore preferito:")
+console.log(favoriteColor)
+
+
+
